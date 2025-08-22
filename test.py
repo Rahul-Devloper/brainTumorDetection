08c2ea2be7a8128229f4b89c36ad82775b11ac5e
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-08-22 01:49:05 +0100

    added api testing to test.py

--- test.py
+++ test.py
@@ -1,32 +1,36 @@
-import os
-import shutil
+# import os
+# import shutil
 
-# Original dataset folders
-SOURCE_DIR = 'input'
-DEST_DIR = 'data/binary_split'
+# # Original dataset folders
+# SOURCE_DIR = 'input'
+# DEST_DIR = 'data/binary_split'
 
-tumor_labels = ['glioma', 'meningioma', 'pituitary']
-no_tumor_label = 'notumor'
+# tumor_labels = ['glioma', 'meningioma', 'pituitary']
+# no_tumor_label = 'notumor'
 
-# Create the target folders
-for split in ['train', 'test']:
-    for category in ['tumor', 'no_tumor']:
-        os.makedirs(os.path.join(DEST_DIR, split, category), exist_ok=True)
+# # Create the target folders
+# for split in ['train', 'test']:
+#     for category in ['tumor', 'no_tumor']:
+#         os.makedirs(os.path.join(DEST_DIR, split, category), exist_ok=True)
 
-# Function to copy images to new binary structure
-def copy_images(split, original_label, target_label):
-    src_folder = os.path.join(SOURCE_DIR, 'Training' if split == 'train' else 'Testing', original_label)
-    dest_folder = os.path.join(DEST_DIR, split, target_label)
-    for img_name in os.listdir(src_folder):
-        src = os.path.join(src_folder, img_name)
-        dst = os.path.join(dest_folder, img_name)
-        if os.path.isfile(src):
-            shutil.copy(src, dst)
+# # Function to copy images to new binary structure
+# def copy_images(split, original_label, target_label):
+#     src_folder = os.path.join(SOURCE_DIR, 'Training' if split == 'train' else 'Testing', original_label)
+#     dest_folder = os.path.join(DEST_DIR, split, target_label)
+#     for img_name in os.listdir(src_folder):
+#         src = os.path.join(src_folder, img_name)
+#         dst = os.path.join(dest_folder, img_name)
+#         if os.path.isfile(src):
+#             shutil.copy(src, dst)
 
-# Process Training and Testing
-for split in ['train', 'test']:
-    for label in tumor_labels:
-        copy_images(split, label, 'tumor')
-    copy_images(split, no_tumor_label, 'no_tumor')
+# # Process Training and Testing
+# for split in ['train', 'test']:
+#     for label in tumor_labels:
+#         copy_images(split, label, 'tumor')
+#     copy_images(split, no_tumor_label, 'no_tumor')
 
-print("✅ Dataset reorganized for binary classification (Tumor / No Tumor).")
+# print("✅ Dataset reorganized for binary classification (Tumor / No Tumor).")
+import requests
+with open(r"D:\ML_Projects\brainTumorDetection\input\Testing\notumor\Te-no_0040.jpg", "rb") as f:
+    r = requests.post("http://localhost:8080/predict", files={"file": f})
+print(r.json())
