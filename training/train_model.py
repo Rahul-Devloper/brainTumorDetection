bc7aa22dd6ad1b7299d32b86a7c5ddca4b76051b
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-09-01 23:20:38 +0100

    added comment

--- training/train_model.py
+++ training/train_model.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 
-test_dir = Path("../data/binary_split/test")
+test_dir = Path("../data/binary_split/test")  # your test dir
+# your model path
 model_path = Path("../notebooks/api/model/brain_mri_model.h5")
 
 print("Test dir exists? ", test_dir.exists())
